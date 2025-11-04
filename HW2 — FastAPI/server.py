7700b5de6e6829c625087b6ff3f047004485a410
Nikita Paniukhin <git@npanuhin.me>
2025-11-04 02:38:34 +0100

    Refactoring old HWs

--- HW2 — FastAPI/server.py
+++ HW2 — FastAPI/server.py
@@ -1,5 +1,3 @@
-from typing import Union
-
 from fastapi import FastAPI, File, UploadFile
 
 app = FastAPI()
@@ -9,17 +7,21 @@ app = FastAPI()
 def get_example():
     return {"message": "GET request received"}
 
+
 @app.post("/post")
 def post_example(data: dict):
     return {"message": "POST request received", "data": data}
+
 
 @app.put("/put/{item_id}")
 def put_example(item_id: int, data: dict):
     return {"message": f"PUT request received for item {item_id}", "data": data}
 
+
 @app.delete("/delete/{item_id}")
 def delete_example(item_id: int):
     return {"message": f"DELETE request received for item {item_id}"}
+
 
 @app.post("/uploadfile/")
 async def create_upload_file(file: UploadFile = File(...)):
